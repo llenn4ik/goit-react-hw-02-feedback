1fec741d34a23a8d4bcbdda1fdf095610e92ca09
llenn4ik <llenn4ik@gmail.com>
2020-10-15 18:33:27 +0200

    Updates

---
+++ precache-manifest.08b25e267b5cc98856b3a53f79bdd79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06dbadeb3cb49f984bc9a4c1ce3be69d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "24f8c33cd2b40acf949e",
+    "url": "/goit-react-hw-02-feedback/static/css/main.58b31ade.chunk.css"
+  },
+  {
+    "revision": "336c6b71e327d3126b2b",
+    "url": "/goit-react-hw-02-feedback/static/js/2.77ef8fd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.77ef8fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f8c33cd2b40acf949e",
+    "url": "/goit-react-hw-02-feedback/static/js/main.02add295.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
